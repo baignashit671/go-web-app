cafd1564a934d4ada560098b7b66f418914a208b
root <root@ip-172-31-23-72.ec2.internal>
2025-03-24 09:51:19 +0000

    feat: implemented CI

--- main.go
+++ main.go
@@ -1,39 +1,41 @@
 package main
 
 import (
-	"log"
-	"net/http"
+    "fmt"
+    "log"
+    "net/http"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	// Render the home html page from static folder
-	http.ServeFile(w, r, "static/home.html")
+    fmt.Println("Home page accessed") // Debug Log
+    http.ServeFile(w, r, "static/home.html")
 }
 
 func coursePage(w http.ResponseWriter, r *http.Request) {
-	// Render the course html page
-	http.ServeFile(w, r, "static/courses.html")
+    fmt.Println("Courses page accessed") // Debug Log
+    http.ServeFile(w, r, "static/courses.html")
 }
 
 func aboutPage(w http.ResponseWriter, r *http.Request) {
-	// Render the about html page
-	http.ServeFile(w, r, "static/about.html")
+    fmt.Println("About page accessed") // Debug Log
+    http.ServeFile(w, r, "static/about.html")
 }
 
 func contactPage(w http.ResponseWriter, r *http.Request) {
-	// Render the contact html page
-	http.ServeFile(w, r, "static/contact.html")
+    fmt.Println("Contact page accessed") // Debug Log
+    http.ServeFile(w, r, "static/contact.html")
 }
 
 func main() {
+    fmt.Println("Starting server on port 8080...")  // Debug Log
+    http.HandleFunc("/home", homePage)
+    http.HandleFunc("/courses", coursePage)
+    http.HandleFunc("/about", aboutPage)
+    http.HandleFunc("/contact", contactPage)
 
-	http.HandleFunc("/home", homePage)
-	http.HandleFunc("/courses", coursePage)
-	http.HandleFunc("/about", aboutPage)
-	http.HandleFunc("/contact", contactPage)
-
-	err := http.ListenAndServe("0.0.0.0:8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+    err := http.ListenAndServe("0.0.0.0:8080", nil)
+    if err != nil {
+        log.Fatalf("Server failed to start: %v", err)  // Improved Error Log
+    }
 }
+
